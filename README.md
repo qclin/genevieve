6dd89a60b4523a558beedcbf778075e98cff523b
Qiao Lin <qiaoclin@gmail.com>
2015-03-31 19:41:00 -0400

    submision of checkinApp

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-give each client a key to telnet 
 
-a module that contains event data: date & topic 
-broadcast eventData.txt when first connected 
+
+a file that contains event data: date & topic 
+
+publish eventData when attendee first connected 
+
 if client would like to attend 
 asked for names and email addresses 
 check for name === 'string', First and LAST
@@ -10,9 +12,37 @@ check for email === string containing @ and .
 	prompts for correction
 keep a head count  var count = 0;
 after name and email is verified, both true, count increments 
-broadcast count to each client 
+
+publish count to each client that connects
+
 save attendee info on separate file 
 
-a module file containing event data 
+allow Geneive to have special access with a passcode 
 
-a rsvp log that clears out once the date is greater that even date 
+when passcode is enter give option list of possible actions
+
+actions include: change event TOPIC & event DATES, to see the list of attendee information, and to also clear the list of RSVP for the next event 
+
+
+////// instructions for genevieve 
+
+upon connection you access code is "/gen"
+you will then be able to choose a list of actions 
+
+"change DATE or TOPIC, see LIST or CLEAR list for the next event"
+
+even command is in listed in UPPERCASE, 
+
+please enter in the following format 
+
+<access code> + <command> + <information>  please maintain information without space breaks or else you'll loose data 
+
+for example if you would like to change the event TOPIC 
+please enter "/gen topic ICE_CREAM_SOCIAL"
+
+and if you would like to change the date 
+please enter "/gen date Week3_d02"
+
+for LIST and Clear simply enter after your access code 
+
+
--- invitation.js
+++ invitation.js
@@ -1,6 +1,5 @@
 var net = require("net");
 var fs = require("fs");
-var event = require("./eventData.js");
 var port = 3000;
 
 var names = [];
@@ -8,49 +7,105 @@ var emails=[];
 var count = 0;
 var at = /[@]/;
 
+/// admin verifications 
+var key = "/gen";
+var login = false;
+var guest = false;
+
+var eTopic=[];
+var eDate=[];
+
+fs.readFile("eventInfo.json",function(err,data){
+	 if(err){console.log(err)}else{
+		var info = JSON.parse(data);
+		 eTopic.push(info[0]);
+		 eDate.push(info[1]);
+	}
+
+});
+// var meetup = new Event(20150331, "cocktail social ");
+
 var server = net.createServer(function(socket){
 	console.log("somebody is here");
-	broadcast("current attendance "+ count);
-socket.write("Join us at "+ event() +"Please RSVP with your First and last name >> ");
+
+
+	socket.write(eTopic+" is happening on "+eDate+" current attendance count is "+ count + " people. Won't you join us? ");
 
 socket.on('data',function(data){
 	var response = data.toString().trim();
-	var split = response.split(" ");
-		if (split.length === 2){
+	var checkRes = response.split(" ");
+	// console.log(checkRes);
+	if(checkRes[0] ===key){
+		login = true; 
+		if (checkRes.length===1){
+		socket.write("Hey girl what'sUp? :change DATE or TOPIC, see LIST or CLEAR list for the next event? >>");
+		}else if(checkRes.length > 1 && login === true){
+		var command = checkRes[1].toUpperCase();
+		console.log(command);
+			if (checkRes[0]===key && command === "DATE"){
+				eDate = checkRes[2];
+				console.log( " date is now " + eDate);
+			}else if(checkRes[0]===key && command==="TOPIC"){
+				eTopic = checkRes[2];
+				console.log(eTopic);
+			}else if(checkRes[0]===key && command ==="LIST"){
+				
+				fs.readFile("rsvp.json",function(err,data){
+					if (err){
+						console.log("something is wrong");
+					}else{
+						socket.write(data);
+					}
+				});
+			}else if(checkRes[0]===key && command ==="CLEAR"){
+				fs.unlink("rsvp.json",function(err){
+					if(err){
+						console.log(err);
+					}else{
+						console.log("RSVP list cleared for the next event");}
+				});
+			}
+			/// write her changes into the json file/// test for readFile line 19
+			var newInfo = [eTopic,eDate];
+			console.log(newInfo);
+			var JnewInfo = JSON.stringify(newInfo);
+
+			fs.writeFile("eventInfo.json",JnewInfo,function(err){
+						console.log("thing changed");
+					}); 
+		}
+
+	}else{
+		if (checkRes[0].toLowerCase()==="yes"){
+		guest = true;
+		socket.write("Please RSVP with your First and last name >> ");
+		}else if (checkRes[0].toLowerCase()==="no"){
+			socket.write("Who are you POSER (╯°□°)╯︵ ┻━┻");
+		}
+		if (checkRes[0] != key && checkRes.length === 2){
 			names.push(response);
 			socket.write("Great, now we just need your email address to RSVP >> ");
 			console.log(names);
 		}
-		if(response.indexOf("@") != -1){
+		if(checkRes[0] != key && response.indexOf("@") != -1){
 			emails.push(response);
 			count ++; 
-			console.log(emails);
-			console.log(count);
+			var contacts = [];
+		for (var i = 0; i<names.length; i++){
+			contacts.push(names[i]+" , "+emails[i]);
 		}
-	var contacts = [];
-	for (var i = 0; i<names.length; i++){
-		contacts.push(names[i]+" , "+emails[i]);
+		console.log(contacts);
+		var Jfile = JSON.stringify(contacts);
+		fs.writeFile("rsvp.json",Jfile,function(err,data){
+				if (err){ console.log(err); }else{socket.write("See you at the party ヾ(⌐■_■)ノ♪ ")}
+			});
+
+		}
 	}
 
-	var Jfile = JSON.stringify(contacts);
-	fs.writeFile("rsvp.json",Jfile,function(err,data){
-			if (err){ console.log(err); }else{}
-		});
+	
 	});
-
-// Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
-      // Don't want to send it to sender
-      if (client === sender) return;
-      client.write(message);
-    });
-    // Log it to the server output too
-    process.stdout.write(message)
-  }
-
 });
-
 
 server.listen(port, function(){
 	console.log("listening on port"+port);
